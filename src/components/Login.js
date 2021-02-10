1cdcbc292778981ab55a535fd1ead0066342bb32
yatharth <y.s.saxena7@gmail.com>
2021-02-10 17:03:40 +0530

    Api manager completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,149 +3,87 @@ import { GoogleLogin } from 'react-google-login'
 import { refreshTokenSetup } from '../utils/refreshToken'
 import { CLIENT_ID, SCOPES } from '../utils/googleCredentials'
 import Dexie from 'dexie'
+import { ApiManager } from '../utils/apiManager'
 
 class Login extends Component {
     
     constructor(props){
         super(props);
+        this.state = {
+            dbInstance: null,
+        }
+    }
+
+    connectIndexedDB = () => {
         var db = new Dexie("flockdev07@gmail");
         db.version(1).stores({
             labels: 'id',
             threads: 'id',
             messages: 'id',
         })
-        this.state = {
-            dbInstance: db,
-        }
+        this.setState({ dbInstance: db });
     }
 
-    // Setting labels
-    fetchLabelsIndexedDB = (fetchLabelAPI, setLabels, dbInstance) => {
-        Dexie.exists("flockdev07@gmail").then(function(exists) {
-            if (exists){
-                new Dexie("flockdev07@gmail").open().then(function(db){
-                    if(db.table("labels")) {
-                            const table_info = db.table("labels").toArray().then(function(details){
-                            setLabels(details);
-                        });
-
-                    }
-                    else{
-                        fetchLabelAPI(setLabels, dbInstance)
-                    }
-                })
+    serachDB = async (dbName, tableName) => {
+        const exist = await Dexie.exists(dbName);
+        if(exist){
+            const db = await new Dexie(dbName).open();
+            if(db.table(tableName)) {
+                const details = await db.table(tableName).toArray()
+                return details;
             }
             else{
-                fetchLabelAPI(setLabels, dbInstance)
+                return null;
             }
-        })
-    }
-
-
-    fetchLabelAPI = (setLabels, dbInstance) => {
-        var request = new XMLHttpRequest()
-        request.open('GET', 'https://gmail.googleapis.com/gmail/v1/users/me/labels');
-        request.setRequestHeader('Authorization', 'Bearer ' + this.props.accessToken);
-        request.onload = function (res) {
-            const labelString = res.currentTarget.response;
-            const labelJson = JSON.parse(labelString);
-            setLabels(labelJson.labels);
-            labelJson.labels.map((value, index) => {
-                dbInstance.labels.put({ ...value })
-            })
         }
-        request.send()
+        else{
+            return null;
+        }    
     }
 
-    setLabels = (labels) => {
-        this.props.setLabels(labels);
-    }
-
-    // Setting threads
-    fetchMailsIndexedDB = (fetchThreadAPI, fetchThreadDetailsAPI, setThread, fetchMessageDetails, dbInstance) => {
-        Dexie.exists("flockdev07@gmail").then(function(exists) {
-            if (exists){
-                new Dexie("flockdev07@gmail").open().then(function(db){
-                    if(db.table("threads")) {
-                                const table_info = db.table("threads").toArray().then(function(details){
-                                    details.map((value, index) => {
-                                        setThread(value);
-                                    })
-                                });
-
-                    }
-                    else{
-                        fetchThreadAPI(fetchThreadDetailsAPI, setThread, fetchMessageDetails, dbInstance)
-                    }
-                })
-            }
-            else{
-                fetchThreadAPI(fetchThreadDetailsAPI, setThread, fetchMessageDetails, dbInstance)
-            }
-        })
-    }
-
-    fetchThreadAPI = (fetchThreadDetailsAPI, setThread, fetchMessageDetails, dbInstance) => {
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://gmail.googleapis.com/gmail/v1/users/me/threads');
-        request.setRequestHeader('Authorization', 'Bearer ' + this.props.accessToken);
-        request.onload = function (res) {
-            const threadJson = JSON.parse(res.currentTarget.response);
-            const threadArray = threadJson.threads;
-            fetchThreadDetailsAPI(threadArray, setThread, fetchMessageDetails, dbInstance);
-        }
-        request.send()
-    }
-
-    fetchThreadDetailsAPI = (threadArray, setThread, fetchMessageDetails, dbInstance) => {
-        for(let i = 0; i < threadArray.length; i++){
-            var request = new XMLHttpRequest()
-            request.open('GET', "https://gmail.googleapis.com/gmail/v1/users/me/threads/"+threadArray[i]["id"]);
-            request.setRequestHeader('Authorization', 'Bearer ' + this.props.accessToken);
-            request.onload = function (res) {
-                const threadDetailsString = res.currentTarget.response;
-                const threadDetailsJson = JSON.parse(threadDetailsString);
-                setThread(threadDetailsJson);
-                fetchMessageDetails(threadDetailsJson.messages, dbInstance);
-                dbInstance.threads.put({ ...threadDetailsJson })
-            }
-            request.send()
-        }
-    }
-
-    fetchMessageDetails = (messageArray, dbInstance) => {
-        messageArray.map((value, index) => {
-            var request = new XMLHttpRequest()
-            request.open('GET', "https://gmail.googleapis.com/gmail/v1/users/me/messages/"+value.id);
-            request.setRequestHeader('Authorization', 'Bearer ' + this.props.accessToken);
-            request.onload = function (res) {
-                const messageString = res.currentTarget.response;
-                const messageJson = JSON.parse(messageString);
-                dbInstance.messages.put({ ...messageJson })
-            }
-            request.send()
-        })
-    }
-    
-    setThread = (threadDetails) => {
-        this.props.setThreadDetails(threadDetails);
-    }
-
-
-    handleResponse = (response) => {
+    handleResponse = async (response) => {
         if (!response.hasOwnProperty('accessToken')){
             return;
         }
         this.props.setAccessToken(response.tokenObj.access_token);
         this.props.setSignedInState({ signedIn: true });
-        this.fetchLabelsIndexedDB(this.fetchLabelAPI, 
-            this.setLabels, 
-            this.state.dbInstance)
-        this.fetchMailsIndexedDB(this.fetchThreadAPI, 
-            this.fetchThreadDetailsAPI, 
-            this.setThread, 
-            this.fetchMessageDetails,
-            this.state.dbInstance)
+
+        this.connectIndexedDB();
+        const apiManager = new ApiManager("me", this.props.accessToken);
+        const labelData = await this.serachDB("flockdev07@gmail", "labels");
+        if(labelData){
+            this.props.setLabels(labelData);
+        }
+        else{
+            apiManager.fetchAPI("labels", "", this.props.accessToken).then((labelJson) => {
+                this.props.setLabels(labelJson.labels);
+                labelJson.labels.map((value) => {
+                    this.state.dbInstance.labels.put({ ...value })
+                })
+            })
+        }
+
+        const threadData = await this.serachDB("flockdev07@gmail", "threads");
+        if(threadData){
+            threadData.map(value => {
+                this.props.setThreadDetails(value);
+            })
+        }
+        else{
+            apiManager.fetchAPI("threads", "").then((threadJson) => {
+                threadJson.threads.map(value => {
+                    apiManager.fetchAPI("threads", value.id).then((threadDetailJson => {
+                        this.props.setThreadDetails(threadDetailJson);
+                        this.state.dbInstance.threads.put({ ...threadDetailJson });
+                        threadDetailJson.messages.map(value => {
+                            apiManager.fetchAPI("messages", value.id).then(messageDetailJson => {
+                                this.state.dbInstance.messages.put({ ...messageDetailJson })
+                            })
+                        })
+                    }))
+                })
+            })
+        }
         refreshTokenSetup(response);
     }
 
